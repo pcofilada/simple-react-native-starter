dc0ae4caa09682015d5639a24895416e1f0756e5
Patrick Ofilada <pcofilada@gmail.com>
2017-02-10 19:52:47 +0800

    Change ex-navigation to react-navigation

--- src/Store.js
+++ src/Store.js
@@ -1,14 +1,8 @@
 import { createStore, applyMiddleware } from 'redux';
 import ReduxThunk from 'redux-thunk';
-import { createNavigationEnabledStore } from '@exponent/ex-navigation';
 
 import reducers from './reducers';
 
-const createStoreWithNavigation = createNavigationEnabledStore({
-  createStore,
-  navigationStateKey: 'navigation',
-});
-const createStoreWithMiddleware = applyMiddleware(ReduxThunk)(createStoreWithNavigation);
-const store = createStoreWithMiddleware(reducers);
+const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
 
 export default store;
